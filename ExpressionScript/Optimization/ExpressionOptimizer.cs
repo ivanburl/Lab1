4cf1477ebb332e41a26d1a12a9b9805af82d2c23
Ivan Burlutskiy <ivandr2004@gmail.com>
2022-10-19 08:57:02 +0200

    Optimization

--- ExpressionScript/Optimization/ExpressionOptimizer.cs
+++ ExpressionScript/Optimization/ExpressionOptimizer.cs
@@ -1,57 +1,60 @@
-﻿using System.Text.RegularExpressions;
-using ExpressionScript.PreProcessing;
+﻿using ExpressionScript.Data;
+using ExpressionScript.Data.Model;
+using ExpressionScript.Formatting;
 using ExpressionScript.Validation;
+using ExpressionScript.Validation.Validator;
 
 namespace ExpressionScript.Optimization;
 
-public class ExpressionOptimizer : IOptimizer<String,List<ExpressionElement>>
+public class ExpressionOptimizer : IOptimizer<string, List<ExpressionElement>>
 {
+    private readonly IValidator<string, ExpressionElementType> _complexExpressionElementValidator;
+    private readonly IFormatter _expressionPreprocessing;
+    private readonly IValidator<string, ExpressionElementType> _simpleExpressionElementValidator;
+
     public ExpressionOptimizer()
     {
         _complexExpressionElementValidator = new ComplexExpressionElementValidator();
         _simpleExpressionElementValidator = new SimpleExpressionElementValidator();
-        _expressionPreprocessing = new ExpressionPreprocessing();
+        _expressionPreprocessing = new ExpressionFormatter();
     }
 
-    private readonly IValidator<String, ExpressionElementType> _complexExpressionElementValidator;
-    private readonly IValidator<String, ExpressionElementType> _simpleExpressionElementValidator;
-    private readonly IPreprocessingExpression _expressionPreprocessing;
-    
-    public List<ExpressionElement> Optimize(String expression)
+    public List<ExpressionElement> Optimize(string expression)
     {
-        var cleanExpression = _expressionPreprocessing.Clean(expression);
+        var cleanExpression = _expressionPreprocessing.Format(expression);
         if (cleanExpression[^1] != ' ') cleanExpression += ' ';
-        
+
         var res = new List<ExpressionElement>();
-        var currentElement = String.Empty;
+        var currentElement = string.Empty;
         var simpleValidation = ExpressionElementType.UndefinedElement;
         var complexValidation = ExpressionElementType.UndefinedElement;
-        
-        for (int i = 0; i < cleanExpression.Length; i++)
+
+        for (var i = 0; i < cleanExpression.Length; i++)
         {
             currentElement += cleanExpression[i];
-            ExpressionElementType simpleValidationFlag = _simpleExpressionElementValidator.ValidationResult(currentElement);
-            ExpressionElementType complexValidationFlag = _complexExpressionElementValidator.ValidationResult(currentElement);
+            var simpleValidationFlag = _simpleExpressionElementValidator.ValidationResult(currentElement);
+            var complexValidationFlag = _complexExpressionElementValidator.ValidationResult(currentElement);
 
-            if (simpleValidationFlag == (ExpressionElementType)(-1) && 
+            if (simpleValidationFlag == (ExpressionElementType)(-1) &&
                 complexValidationFlag == (ExpressionElementType)(-1))
             {
                 currentElement = currentElement.Substring(0, currentElement.Length - 1);
                 if (simpleValidation >= (ExpressionElementType)1)
-                    res.Add(new(currentElement, simpleValidation));
+                    res.Add(new ExpressionElement(currentElement, simpleValidation));
                 else if (complexValidation >= (ExpressionElementType)1)
-                    res.Add(new(currentElement, complexValidation));
-                else throw new Exception("Undefined part of expression " + currentElement);
-                
-                currentElement = String.Empty;
+                    res.Add(new ExpressionElement(currentElement, complexValidation));
+                else throw new InvalidOperationException("Undefined part of expression " + currentElement + " " + i);
+
+                currentElement = string.Empty;
                 if (cleanExpression[i] != ' ') i--;
                 simpleValidationFlag = ExpressionElementType.UndefinedElement;
                 complexValidationFlag = ExpressionElementType.UndefinedElement;
             }
-            
+
             simpleValidation = simpleValidationFlag;
             complexValidation = complexValidationFlag;
         }
+
         return res;
     }
 }
