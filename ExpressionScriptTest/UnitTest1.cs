b2f048143b9a6c15715d58ee86d3c6747e7d0a27
Ivan Burlutskiy <ivandr2004@gmail.com>
2022-10-19 08:56:16 +0200

    Small tests

--- ExpressionScriptTest/UnitTest1.cs
+++ ExpressionScriptTest/UnitTest1.cs
@@ -1,30 +1,60 @@
+using ExpressionScript.Compilation;
+using ExpressionScript.Compilation.Compiler;
+using ExpressionScript.Data;
+using ExpressionScript.Data.Model;
 using ExpressionScript.Optimization;
-using ExpressionScript.Validation;
+using ExpressionScript.Validation.Validator;
 
 namespace ExpressionScriptTest;
 
 public class Tests
 {
-    private ExpressionOptimizer _optimizer;
-    private ExpressionSyntaxValidator _validator;
+    private ExpressionStringCompiler _compiler;
+    private Dictionary<ExpressionElement, ExpressionElement> constants = new ();
     [SetUp]
     public void Setup()
     {
-        _validator = new ExpressionSyntaxValidator();
-        _optimizer = new ExpressionOptimizer();
+        constants.Add(new ExpressionElement("A11", ExpressionElementType.Constant), new ExpressionElement("10", ExpressionElementType.Number));
+        _compiler = new ExpressionStringCompiler(constants);
     }
 
     [Test]
     public void Test1()
     {
-        String code1 = "div(div(mod(10,20),40),mmax(10,20,30))";
-        var res = _optimizer.Optimize(code1);
-        Console.WriteLine(res.Count);
-        foreach (var i in res)
-        {
-            Console.WriteLine(i.ToString());
-        }
-        
-        Console.WriteLine(_validator.ValidationResult(code1));
+        String code1 = "div(A11,2)";
+        var res = _compiler.Compile(code1);
+        Console.WriteLine(res.Invoke());
+    }
+
+    [Test]
+    public void Test2()
+    {
+        String code2 = "(div(4,1)>2) = true";
+        var res = _compiler.Compile(code2);
+        Console.WriteLine(res.Invoke());
+    }
+    
+    [Test]
+    public void TestSimple()
+    {
+        String code2 = "div(100,mod(10,20))";
+        var res = _compiler.Compile(code2);
+        Console.WriteLine(res.Invoke());
+    }
+
+    [Test]
+    public void Test3()
+    {
+        String code2 = "mmax(10,20,-10, 1000000000)";
+        var res = _compiler.Compile(code2);
+        Console.WriteLine(res.Invoke());
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        String code2 = "-100";
+        var res = _compiler.Compile(code2);
+        Console.WriteLine(res.Invoke());
     }
 }
